8618da29c76d6c2bd483c493ec4065f65dba951c
Ayayayayayayayaya <84789294+NightsTimeZ@users.noreply.github.com>
2024-09-17 07:33:19 +0700

    Update N-Queens.py
    improve ;> so good now use backtracking

--- hard/N-Queens.py
+++ hard/N-Queens.py
@@ -1,49 +1,35 @@
-import copy
-
+import pickle
+# best ;> very simple
 class Solution:
+    def addtheredzone(self,board,row,colum,n):
+        for i in range(0,n):
+            board[row][i] = "X"
+            board[i][colum] = "X"
+            j = colum - (row - i)
+            if 0 <= j < n:
+                board[i][j] = "X"
+            j2 = colum + (row - i)
+            if 0 <= j2 < n:
+                board[i][j2] = "X"
+    def deepcopy(self,board):
+        return pickle.loads(pickle.dumps(board))
     def solveNQueens(self, n: int) -> List[List[str]]:
         if n == 1:
             return [["Q"]]
-        board = [["."] * n for _ in range(n)]
-        def getthehitbox(board, row, col):
-            # if col in setblacklist:
-            #     return False
-
-            for i in range(0,row):
-                boardikub = board[i]
-                if boardikub[col] == 'Q':
-                    return False
-                j = col - (row - i)
-                if 0 <= j < n and boardikub[j] == 'Q':
-                    return False
-                j2 = col + (row - i)
-                if 0 <= j2 < n and boardikub[j2] == 'Q':
-                    return False
-            
-            return True
-
-        allkeep = {i: [] for i in range(n + 1)}
-        allkeep[0] = [board]
-        formatboard = []
-        for w in range(1,n+1):
-            old_w = w-1
-            if old_w != 0:
-                allkeep[w-2] = []
-                
-            allkeep_old = allkeep[old_w]
-            for i in range(0,len(allkeep_old)):
-                for j in range(0,n):
-                    cloneboard = (allkeep_old[i])
-                    allkeep[w-1] = []
-                    if old_w == 0 or getthehitbox(cloneboard,old_w,j):
-                        if w != n:
-                            cloneboard = copy.deepcopy(cloneboard)
-                        cloneboard[old_w][j] = "Q"
-                        if w == n:
-                            formatboard.append([''.join(row) for row in cloneboard])
-                        else:
-                            allkeep[w].append(cloneboard)
         
-        allkeep = {}
-        board = []
-        return formatboard
+        board = [["."]*n for _ in range(n)]
+        res = []
+        def backtrack_maybe(board,row):
+            if row == n:
+                res.append([''.join('.' if cell == 'X' else cell for cell in row) for row in board])
+                return
+            for col in range(0,n):
+                if board[row][col] == ".":
+                    cloneboard = board
+                    if row != n-1:
+                        cloneboard = self.deepcopy(cloneboard)
+                        self.addtheredzone(cloneboard,row,col,n)
+                    cloneboard[row][col] = "Q"
+                    backtrack_maybe(cloneboard,row+1)
+        backtrack_maybe(board,0)
+        return res
